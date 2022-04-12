eb7f92c3a8f8567f8a8f3c0c502d18be3a741b7e
Dilmer Valecillos <dilmervst@gmail.com>
2022-04-11 20:52:41 -0600

    Added example 2 with meshing introduction

---
+++ Assets/Scripts/ARProjectileManager.cs
@@ -0,0 +1,34 @@
+using Niantic.ARDK.Utilities;
+using UnityEngine;
+
+public class ARProjectileManager : MonoBehaviour
+{
+    [SerializeField]
+    private Camera arCamera;
+
+    [SerializeField]
+    private GameObject[] projectilesPrefabs;
+
+    [SerializeField]
+    private float force = 50.0f;
+    
+    void Update()
+    {
+        if (PlatformAgnosticInput.touchCount <= 0) { return; }
+
+        var touch = PlatformAgnosticInput.GetTouch(0);
+        if (touch.phase == TouchPhase.Began)
+        {
+            LaunchRandomProjectile();
+        }
+    }
+
+    private void LaunchRandomProjectile()
+    {
+        var prefab = projectilesPrefabs[Random.Range(0, projectilesPrefabs.Length)];
+        var projectile = Instantiate(prefab, arCamera.transform.position, Quaternion.identity);
+
+        var projectileRigidBody = projectile.GetComponent<Rigidbody>();
+        projectileRigidBody.AddForce(arCamera.transform.forward * force);
+    }
+}
